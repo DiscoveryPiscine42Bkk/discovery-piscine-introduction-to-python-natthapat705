0135b3c0011a3f7228a593a1f200d087562961b9
natthapat705 <earnnat2550@gmail.com>
2025-07-20 12:17:31 +0000

    calculator

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -1,7 +1,6 @@
-# รับค่าตัวเลขตัวแรกจากผู้ใช้ และแปลงเป็นจำนวนเต็ม (int)
-num1 = int(input("Give me the first number: "))
-num2 = int(input("Give me the second number: "))
-print('Thank You')
+num1 = int(input("กรอกตัวเลข: "))
+num2 = int(input("กรอกตัวเลข: "))
+print('ขอบคุณที่กรอกตัวเลขเข้ามา')
 # แสดงผลการบวกของ num1 และ num2
 print(f"{num1} + {num2} = {num1 + num2}")
 
